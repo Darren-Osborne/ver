dd73a98ed9d99b9b10d0d5d07650def0e50b67a4
Darren-Osborne <darren.osborne@outlook.com>
2021-12-09 10:21:56 +0000

    added SubtractionService (#4)
    Co-authored-by: Darren <darren.osborne@rtgs.com>

---
+++ OsMath/OsMathService/SubtractionService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OsMathService
+{
+    public class SubtractionService
+    {
+        public double subtract(double lhs, double rhs)
+        {
+            return lhs - rhs;
+        }
+    }
+}
